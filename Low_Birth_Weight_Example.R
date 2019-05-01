a78215f776df6b941ae1178110a49516ad1d6d0e
autumnvalicevic <autumn.valicevic@gmail.com>
2019-05-01 15:22:22 -0400

    Fixed Issues

--- Low_Birth_Weight_Example.R
+++ Low_Birth_Weight_Example.R
@@ -1,5 +1,7 @@
 ### Example of Logistic Regression Using dataset from class
 require(ResourceSelection)
+require(gmodels)
+require(multcomp)
 
 ## Read in data
 lbw <- read.table("~/Downloads/LBW.txt", quote="\"", comment.char="")
@@ -24,7 +26,7 @@ table(lbw$ftv)
 table(lbw$ptl)
 
 ## b) Fit main effects model based on all covariates
-lbw.mod1 <- glm(low ~ age + wt + racew + raceb + smoke + hyp + ui + ftv + ptl,family = "binomial" ,data = lbw)
+lbw.mod1 <- glm(low ~ age + wt + racew + raceb + smoke + hyp + ui + ftv + ptl,family = binomial(link="logit") ,data = lbw)
 summary(lbw.mod1)
 
 ## c) Carry out the Hosmer-Lemeshow goodness of fit test
@@ -37,7 +39,7 @@ hl.test <- hoslem.test(lbw.mod1$y, fitted(lbw.mod1), g = 10)
 backwards.sel <-step(lbw.mod1)
 formula(backwards.sel)
 
-nothing <- glm(low ~ 1, family = "binomial", data = lbw)
+nothing <- glm(low ~ 1, family = binomial(link="logit"), data = lbw)
 # Forwards selection
 forwards.sel <- step(nothing,
                 scope=list(lower=formula(nothing),upper=formula(lbw.mod1)), direction="forward")
@@ -48,11 +50,11 @@ bothways <- step(nothing, list(lower=formula(nothing),upper=formula(lbw.mod1)),
 formula(bothways)
 
 # Ending model
-lbw.mod2 <- glm(low ~ ptl + hyp + wt + racew + smoke + ui, data = lbw)
+lbw.mod2 <- glm(low ~ ptl + hyp + wt + racew + smoke + ui, family = binomial(link="logit"), data = lbw)
 
 ## e) Re-fit model with age, race, ftv removed. Interprete the smoke and wt parameters and B_0
 
-lbw.mod3 <- glm(low ~ ptl + hyp + wt + smoke + ui, family = "binomial", data = lbw)
+lbw.mod3 <- glm(low ~ ptl + hyp + wt + smoke + ui, family = binomial(link=logit), data = lbw)
 summary(lbw.mod3)
 
 # B_smoke: 0.5035 -> The effect of smoking on the log odds of having low birth weight 
@@ -67,16 +69,21 @@ summary(lbw.mod3)
 ## Compare B_0
 
 lbw$wt_centered <- lbw$wt -  mean(lbw$wt)
-lbw.mod4 <- glm(low ~ ptl + hyp + wt_centered + smoke + ui, family = "binomial", data = lbw)
+lbw.mod4 <- glm(low ~ ptl + hyp + wt_centered + smoke + ui, family = binomial(link=logit), data = lbw)
 summary(lbw.mod4)
 
 # B_0 under the centered model is -1.52 vs. 0.47
 # B_wt and B_wtcentered are the same value
 
 ## g) Carry out a test of whether the effect of smoke depends on either UI or PTL
+lbw$smoke_ui <- as.factor(lbw$smoke * lbw$ui)
+lbw$smoke_ptl <- as.factor(lbw$smoke * lbw$ptl)
 
 
+lbw.mod5 <- glm(low ~ ptl + hyp + wt_centered + smoke + ui + smoke_ui + smoke_ptl, family = binomial(link="logit"), data = lbw)
+summary(lbw.mod5)
 
+fit.contrast(lbw.mod5, "smoke_ui",c(1,-1))
 
 
 
